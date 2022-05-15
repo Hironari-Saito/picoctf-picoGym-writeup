bcc7826c9b4b6dd3207d67a8722942638cdc4c25
Kali <hiroshi31100@gmail.com>
2022-05-14 22:25:25 -0400

    tab tab attack

---
+++ general/tab_tab_attack/exec.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+unzip -o Addadshashanammu.zip > /dev/null
+./Addadshashanammu/Almurbalarammi/Ashalmimilkala/Assurnabitashpi/Maelkashishi/Onnissiralis/Ularradallaku/fang-of-haynekhtnamet
